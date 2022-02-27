9c672a5909a12b833a6e3c82fa45925d2c77fc9d
Darin Campbell - Raarge Actual <animusdrogai@gmail.com>
2022-02-26 21:33:51 -0500

    Created the D20Character class, added in the dicerollerservice to access the added database.

---
+++ DarinsDiceRoller/DarinsDiceRoller/Services/DiceRollerService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Xamarin.Essentials;
+using SQLite;
+using DarinsDiceRoller.Models;
+
+namespace DarinsDiceRoller.Services
+{
+    public static class DiceRollerService
+    {
+        static SQLiteAsyncConnection db;
+        static async Task Init()
+        {
+            if (db != null)
+                return;
+
+            var databasePath = Path.Combine(FileSystem.AppDataDirectory, "D20CharacterDB");
+
+            db = new SQLiteAsyncConnection(databasePath);
+
+            await db.CreateTableAsync<D20Character>();
+
+        }
+
+        public static async Task AddCharacter(String name, int str, int dex, int con, int intl, int wisd, int cha)
+        {
+            await Init();
+
+            var character = new D20Character
+            {
+                Name = name,
+                Strength = str,
+                Dexterity = dex,
+                Constitution = con,
+                Intelligence = intl,
+                Wisdom = wisd,
+                Charisma = cha
+            };
+
+            var id = await db.InsertAsync(character);
+        }
+
+        public static async Task RemoveCharacter(int CharacterId)
+        {
+            await Init();
+
+            await db.DeleteAsync<D20Character>(CharacterId);
+        }
+
+        public static async Task<IEnumerable<D20Character>> GetCharacter()
+        {
+            await Init();
+
+            var characters = await db.Table<D20Character>().ToListAsync();
+
+            return characters;
+        }
+
+    }
+}
---
+++ DarinsDiceRoller/DarinsDiceRoller/ViewModels/CharacterViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using DarinsDiceRoller.Models;
+using MvvmHelpers;
+using MvvmHelpers.Commands;
+using DarinsDiceRoller.Services;
+
+namespace DarinsDiceRoller.ViewModels
+{
+    public class CharacterViewModel
+    {
+        public ObservableRangeCollection<D20Character> Character { get; set; }
+        public AsyncCommand RefreshCommand { get; }
+        public AsyncCommand AddCommand { get; }
+        public AsyncCommand<D20Character> RemoveCommand { get; }
+        public bool IsBusy = false;
+
+        public CharacterViewModel()
+        {
+            Character = new ObservableRangeCollection<D20Character>();
+
+            RefreshCommand = new AsyncCommand(Refresh);
+            AddCommand = new AsyncCommand(Add);
+            RemoveCommand = new AsyncCommand<D20Character>(Remove);
+        }
+
+        async Task Add()
+        {
+            var name = await App.Current.MainPage.DisplayPromptAsync("Name", "Character Name");
+            var strength = await App.Current.MainPage.DisplayPromptAsync("Strength", "Character Strength");
+            var dexterity = await App.Current.MainPage.DisplayPromptAsync("Dexterity", "Character Dexterity");
+            var constitution = await App.Current.MainPage.DisplayPromptAsync("Constitution", "Character Constitution");
+            var Intelligence = await App.Current.MainPage.DisplayPromptAsync("Intelligence", "Character Intelligence");
+            var Wisdom = await App.Current.MainPage.DisplayPromptAsync("Wisdoom", "Character Wisdom");
+            var Charisma = await App.Current.MainPage.DisplayPromptAsync("Charisma", "Character Charisma");
+            await DiceRollerService.AddCharacter(name, Convert.ToInt32(strength), Convert.ToInt32(dexterity),
+                Convert.ToInt32(constitution), Convert.ToInt32(Intelligence), Convert.ToInt32(Wisdom), Convert.ToInt32(Charisma));
+            await Refresh();
+        }
+
+        async Task Remove(D20Character character)
+        {
+            await DiceRollerService.RemoveCharacter(character.CharacterId);
+            await Refresh();
+        }
+
+        async Task Refresh()
+        {
+            IsBusy = true;
+
+            await Task.Delay(2000);
+
+            Character.Clear();
+
+            var characters = await DiceRollerService.GetCharacter();
+
+            Character.AddRange(characters);
+
+            IsBusy = false;
+        }
+    }
+}
